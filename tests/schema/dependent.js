cb9f353586ac2f22a1bde9e4be95c066a694d195
Wyatt Greenway <th317erd@gmail.com>
2017-09-30 19:29:46 -0700

    Updates

--- tests/schema/dependent.js
+++ tests/schema/dependent.js
@@ -1,15 +1,18 @@
-import { defineSchema } from '../../lib/schema';
+import { defineSchema, generateUUID } from '../../lib/schema';
 import { Dependent } from '../models';
 
 module.exports = defineSchema(Dependent, (engine) => {
   return {
-    firstName: '',
-    lastName: String,
-    isDependent: {
-      type: Boolean,
-      field: 'dependent',
-      getValue: () => true,
-      setValue: (model, fieldName) => (model[fieldName] = true)
+    generateID: () => ('DP_' + generateUUID()),
+    fields: {
+      firstName: '',
+      lastName: String,
+      isDependent: {
+        type: Boolean,
+        field: 'dependent',
+        getValue: () => true,
+        setValue: (model, fieldName) => (model[fieldName] = true)
+      }
     }
   }
 });
--- tests/schema/user.js
+++ tests/schema/user.js
@@ -1,10 +1,13 @@
-import { defineSchema, filterSchemaToEngine } from '../../lib/schema';
+import { defineSchema, filterSchemaToEngine, generateUUID } from '../../lib/schema';
 import { User, Dependent } from '../models';
 
 module.exports = defineSchema(User, (engine) => {
-  return filterSchemaToEngine({
-    firstName: { type: String, field: 'first_name', 'field:sql': 'first_name_sql' },
-    lastName: { type: String, field: 'last_name', 'field:sql': 'last_name_sql' },
-    dependents: [Dependent]
-  }, engine)
+  return {
+    generateID: () => ('US_' + generateUUID()),
+    fields: filterSchemaToEngine({
+      firstName: { type: String, field: 'first_name', 'field:sql': 'first_name_sql' },
+      lastName: { type: String, field: 'last_name', 'field:sql': 'last_name_sql' },
+      dependents: [Dependent]
+    }, engine)
+  }
 });
