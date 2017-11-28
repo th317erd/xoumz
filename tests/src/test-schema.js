7b8ee8a96e8aca89758410f0806096b95b55a3a9
Wyatt Greenway <th317erd@gmail.com>
2017-11-27 18:36:29 -0700

    Updates / cleanup

--- tests/src/test-schema.js
+++ tests/src/test-schema.js
@@ -17,17 +17,17 @@ import testPlugin from './test-plugin';
     await myApp.init((app, schema, connectors) => {
       schema.registerModelType('User', User);
       schema.registerModelType('Dependent', Dependent, 'User');
-      connectors.register(new app.ConnectorCollection.MemoryConnector());
+      connectors.register(new app.ConnectorEngine.MemoryConnector());
     });
 
-    var schemaTypes = myApp.getSchema().schemaTypes,
+    var schemaTypes = myApp.getSchemaEngine().schemaTypes,
         testModel = await myApp.createType('User', JSON.stringify({ firstName: 'Test', age: '56.453', items: '45|23|765.345' }));
 
     testModel.save();
 
-    //console.log(inspect(myApp.getSchema().getTypeInfo('Dependent')));
+    //console.log(inspect(myApp.getSchemaEngine().getTypeInfo('Dependent')));
     myApp.Logger.debug('MODEL: ', inspect(testModel));
-    //myApp.Logger.debug('MODEL SCHEMA: ', inspect(testModel), inspect(myApp.getSchema().introspectType({ id: 'USER:1234', firstName: null, age: 65 })));
+    //myApp.Logger.debug('MODEL SCHEMA: ', inspect(testModel), inspect(myApp.getSchemaEngine().introspectType({ id: 'USER:1234', firstName: null, age: 65 })));
 
     var testModelLoaded = await myApp.loadType({ firstName: 'Test' }, { schemaType: 'User' });
     myApp.Logger.debug('MODEL: ', inspect(testModelLoaded));
