8e066c07a06c5f0b116901590f4f6112c2cd2c80
Wyatt Greenway <th317erd@gmail.com>
2017-12-09 21:44:41 -0700

    Updates

--- tests/src/models/user.js
+++ tests/src/models/user.js
@@ -1,7 +1,7 @@
 import userSchema from '../schema/user';
 
-module.exports = function(self, schemaTypes, BaseRecord) {
-  return class User extends BaseRecord {
+module.exports = function(self, schemaTypes, ModelBase) {
+  return class User extends ModelBase {
     static schema(...args) {
       return userSchema.call(this, ...args);
     }
