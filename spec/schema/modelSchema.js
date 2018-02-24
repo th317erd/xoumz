b9e95a9ec7d3f7fd48ab8ee941d581ef0417b684
Wyatt Greenway <th317erd@gmail.com>
2018-02-24 13:52:04 -0700

    WIP schema updates

--- spec/schema/modelSchema.js
+++ spec/schema/modelSchema.js
@@ -11,7 +11,7 @@ describe('Default Schema Types', function() {
   });
 
   it('should be able to define a model schema', function() {
-    var modelSchema = new this.ModelSchema(this.schemaEngine, 'User', this.userSchema);
+    var modelSchema = new this.ModelSchema(this.schemaEngine, { getTypeName: () => 'User', schema: this.userSchema });
 
     var definition = modelSchema.getSchemaDefinition(),
         rawSchema = modelSchema.getSchema();
@@ -26,19 +26,30 @@ describe('Default Schema Types', function() {
     expect(rawSchema.id).toBeTruthy();
     expect(rawSchema.createdAt).toBeTruthy();
     expect(rawSchema.updatedAt).toBeTruthy();
+
+    var idField;
+    for (var [ fieldName, field ] of modelSchema) {
+      if (fieldName === 'id')
+        idField = field;
+    }
+
+    expect(idField).toBeTruthy();
   });
 
   it('should be able to define a model schema that inherits from a parent schema', function() {
-    var modelSchema = new this.ModelSchema(this.schemaEngine, 'User', (defineSchema) => {
-      return defineSchema(this.userSchema, {
-        schema: function({ String }, parentSchema) {
-          return Object.assign(parentSchema, {
-            'derp': String.maxLength(128).nullable(false)
-          });
-        },
-        demote: (model) => model,
-        promote: (model) => model
-      });
+    var modelSchema = new this.ModelSchema(this.schemaEngine, {
+      getTypeName: () => 'User',
+      schema: (defineSchema) => {
+        return defineSchema(this.userSchema, {
+          schema: function({ String }, parentSchema) {
+            return Object.assign(parentSchema, {
+              'derp': String.maxLength(128).nullable(false)
+            });
+          },
+          demote: (model) => model,
+          promote: (model) => model
+        });
+      }
     });
 
     var definition = modelSchema.getSchemaDefinition(),
@@ -54,5 +65,23 @@ describe('Default Schema Types', function() {
     expect(rawSchema.id).toBeTruthy();
     expect(rawSchema.createdAt).toBeTruthy();
     expect(rawSchema.updatedAt).toBeTruthy();
+
+    var derpField;
+
+    // First get version 1
+    for (var [ fieldName, field ] of modelSchema.entries({ version: 1 })) {
+      if (fieldName === 'derp')
+        derpField = field;
+    }
+
+    expect(derpField).toBeFalsy();
+
+    // Next get version 2
+    for (var [ fieldName, field ] of modelSchema) {
+      if (fieldName === 'derp')
+        derpField = field;
+    }
+
+    expect(derpField).toBeTruthy();
   });
 });
